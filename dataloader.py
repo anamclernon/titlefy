e4ddf1c75eb12d4a7da50b03018cea1ad90c3f89
anamclernon <anamclernon@gmail.com>
2022-12-12 20:53:04 +0100

    Made OO

--- dataloader.py
+++ dataloader.py
@@ -1,11 +1,25 @@
 import pandas as pd
 from transformers import RobertaTokenizer
-import tensorflow as tf
 
 
-def load_data(path: str, split=0.8):
-    df = pd.read_csv(path, index_col=0)
-    size = len(df)
-    train_ds = df[:round(size*split)]
-    test_ds = df[round(size*split):]
-    return train_ds, test_ds
+class DataLoader:
+    def __init__(self, file_path: str) -> None:
+        self.dataset = pd.read_csv(path, index_col=0)
+        self.features = self.dataset["content"]
+        self.labels = self.dataset["title"]
+
+    def __len__(self):
+        return len(self.dataset)
+
+    def split_data(self, split=0.8) -> tuple[pd.DataFrame]:
+        train_features = self.features[round(len(self)) :]
+        train_labels = self.labels[round(len(self)) :]
+        test_features = self.features[: round(len(self))]
+        test_labels = self.labels[: round(len(self))]
+        return train_features, train_labels, test_features, test_labels
+
+
+if __name__ == "__main__":
+    path = r"..\titlefy_dataset\small_title_article.csv"
+    data = DataLoader(path)
+    print(data.dataset)
